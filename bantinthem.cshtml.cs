81e5fd50aa1986baccbff2e540b5b81e2dd145ab
lephatdat dpm195550 <115069728+lephatdat2001@users.noreply.github.com>
2022-10-29 19:54:05 +0700

    bản tin thêm

---
+++ bantinthem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace b3.Pages
+{
+    public class bantinthemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
