dfb4fce8d6455e29e61b10859acd2436cd5bad52
Dpm195437 <115070959+Dpm195437@users.noreply.github.com>
2022-11-02 14:48:36 +0700

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace b3.Models
+{
+    public class Class
+    {
+        public int ClassID { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
