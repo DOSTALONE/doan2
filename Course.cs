8a09584f3d63d76f45e0ce1527fb621236d6ef78
Dpm195437 <115070959+Dpm195437@users.noreply.github.com>
2022-11-02 14:47:57 +0700

    Add files via upload

---
+++ Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace b3.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
